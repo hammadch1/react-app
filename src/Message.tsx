e616e57fd82972356db3ee228baa0004ad6c1f42
MUHAMMAD_HAMMAD CHAUDHARY <hammadch1@outlook.com>
2024-02-12 09:24:11 +0100

    not adding a lot of changes

--- src/Message.tsx
+++ src/Message.tsx
@@ -6,10 +6,10 @@
 // PascalCasing
 function Message() {
     // JSX: JavaScript XML
-    const name = ''
+    const name = 'Hammad';
     if (name)
         return <h1>Hello {name}</h1>;
-        return <h1>Hello World</h1>;
+    return <h1>Hello World</h1>;
 }
 
 export default Message;
