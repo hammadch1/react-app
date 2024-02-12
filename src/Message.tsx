c78ed83fe17bce972062091ed458a18f2609a262
MUHAMMAD_HAMMAD CHAUDHARY <hammadch1@outlook.com>
2024-02-12 07:32:19 +0100

    first build

---
+++ src/Message.tsx
@@ -0,0 +1,15 @@
+// 2 ways to create a react component
+// we can use a JS class or a function
+// these days function based components are more popular being concise and easier to write
+// we are using function based component
+
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    const name = ''
+    if (name)
+        return <h1>Hello {name}</h1>;
+        return <h1>Hello World</h1>;
+}
+
+export default Message;
