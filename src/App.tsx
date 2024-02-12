9b827898c4e938c18841412b62904b806991eca3
MUHAMMAD_HAMMAD CHAUDHARY <hammadch1@outlook.com>
2024-02-12 13:12:34 +0100

    final push for react app for just basic things

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button"
+import Alert from "./components/Alert"
+import { useState } from "react"
+
 function App() {
-  return <div><ListGroup/></div>
+  const [alertVisible, setAlertVisibility] = useState(false)
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>I am Button</Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
